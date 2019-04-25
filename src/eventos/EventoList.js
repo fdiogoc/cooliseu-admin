d4abc166d110c5cd2b8250baf25a39a29361a552
Diogo Cardoso Fernandes <diogofernandes@outlook.com>
2019-04-25 19:22:43 -0300

    added id to eventoList

--- src/eventos/EventoList.js
+++ src/eventos/EventoList.js
@@ -4,6 +4,7 @@ import { List, Datagrid, TextField, DateField } from "react-admin";
 const UserList = props => (
   <List {...props}>
     <Datagrid rowClick="edit">
+      <TextField source="id" />
       <TextField source="nome" />
       <TextField source="local" />
       <DateField soruce="data_inicio" />
