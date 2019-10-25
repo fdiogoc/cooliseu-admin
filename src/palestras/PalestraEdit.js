4aa0e1df67eee82d6c163c005f47a912fd3c8e9e
Diogo Cardoso Fernandes <diogofernandes@outlook.com>
2019-10-25 15:11:36 -0300

    Corrigido erros data palestra #closes2

--- src/palestras/PalestraEdit.js
+++ src/palestras/PalestraEdit.js
@@ -8,17 +8,62 @@ import {
   SelectInput
 } from "react-admin";
 
+import { DateInput, TimeInput, DateTimeInput } from "react-admin-date-inputs";
+const dateFormatter = (v) => {
+  if (typeof v.toLocaleString === "function") {
+    if (v.constructor.name == "Timestamp") {
+      return v.toDate();
+    }
+    return new Date(v);
+  }
+  if (v !== undefined) {
+    const dataISO = new Date(v.toDate()).toLocaleString();
+    return dataISO;
+  }
+};
+
+const dateParser = (v) => {
+  if (v == undefined) {
+    return;
+  }
+  if (typeof v.toLocaleString === "function") {
+    if (v.constructor.name == "Timestamp") {
+      return v.toDate();
+    }
+    return new Date(v);
+  }
+  if (v !== undefined) {
+    const dataISO = new Date(v.toDate()).toLocaleString();
+    return dataISO;
+  }
+};
+
 export const PalestraEdit = (props) => (
   <Edit {...props}>
     <SimpleForm>
       <DisabledInput source="id" />
       <TextInput source="tema" />
+      <TextInput source="tema2" />
       <ReferenceInput source="eventoId" reference="eventos">
         <SelectInput optionText="nome" />
       </ReferenceInput>
       <ReferenceInput source="palestranteId" reference="palestrantes">
         <SelectInput optionText="nome" />
       </ReferenceInput>
+
+      <DisabledInput format={dateFormatter} parse={dateParser} source="data" />
+
+      <DateTimeInput
+        format={dateFormatter}
+        parse={dateParser}
+        source="data"
+        label="Data"
+        options={{
+          format: "dd/MM/YYYY, HH:mm:ss",
+          ampm: false,
+          clearable: true
+        }}
+      />
     </SimpleForm>
   </Edit>
 );
